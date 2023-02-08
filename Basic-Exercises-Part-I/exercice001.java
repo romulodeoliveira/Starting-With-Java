d71cee946cd65cb297fc1c11522656e158de02cf
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-02-08 11:22:56 -0300

    resolução do primeiro exercicio

---
+++ Basic-Exercises-Part-I/exercice001.java
@@ -0,0 +1,12 @@
+/*
+Write a Java program to print 'Hello' on screen and then print your name on a separate line.
+Expected Output :
+Hello
+Alexandra Abramov
+*/
+
+class FirstApp {
+    public static void main(String[] args) {
+        System.out.print("Olá, Mundo!\nRômulo de Oliveira\n");
+    }
+}
