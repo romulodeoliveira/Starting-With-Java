3b58ff8304bf117f170d075fef200fe3bd3d6c59
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-02-08 22:53:57 -0300

    resolução do segundo exercicio

---
+++ Basic-Exercises-Part-I/exercice002.java
@@ -0,0 +1,24 @@
+
+/*
+ * Write a Java program to print the sum of two numbers.
+ * 
+ * Test Data:
+ * 74 + 36
+ * Expected Output :
+ * 110
+ */
+import java.util.Scanner; // Aqui importo a classe scanner aqui
+
+class MySum {
+    public static void main(String args[]) {
+        Scanner sum = new Scanner(System.in); // Aqui instancio e crio um objeto Scanner
+
+        System.out.println("Informe o primeiro valor: ");
+        int a = sum.nextInt();
+
+        System.out.print("Informe o segundo valor: ");
+        int b = sum.nextInt();
+
+        System.out.printf("%d + %d = %d", a, b, (a + b));
+    }
+}
