8f9464db59d950bd3d292ab267d87bbf8353cd78
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-02-12 20:36:43 -0300

    Exercicio 02 da lista proposta no S4-E29

---
+++ Course-of-Java-Nelio-Alves/exercices/exercice003.java
@@ -0,0 +1,26 @@
+/*
+ * Faça um programa para ler o valor do raio de um círculo, e depois mostrar o
+ * valor da área deste círculo com quatro casas decimais conforme exemplos.
+ * 
+ * Fórmula da área: area = pi . raio2
+ * Considere o valor de pi = 3.14159
+ */
+
+import java.util.Scanner;
+
+public class exercice003 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double pi = 3.14159;
+        double raio;
+
+        System.out.print("Digite o valor do raio: ");
+        raio = sc.nextDouble();
+
+        double area = pi * Math.pow(raio, 2);
+
+        System.out.printf("%nO valor da área deste cŕculo é de: %.4f%n", area);
+
+        sc.close();
+    }
+}
