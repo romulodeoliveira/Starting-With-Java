71c347bd06dc139e06dbadac9de01b4f6ab75987
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-02-12 20:38:09 -0300

    Exercicio 04 da lista proposta no S4-E29

---
+++ Course-of-Java-Nelio-Alves/exercices/exercice005.java
@@ -0,0 +1,9 @@
+/*
+ * Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
+ */
+
+public class exercice005 {
+    public static void main(String[] args) {
+
+    }
+}
