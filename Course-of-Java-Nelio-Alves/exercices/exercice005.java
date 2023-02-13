7b6f98f6a87c16a7cf0713615b7e13afb67fe7f1
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-02-13 00:02:31 -0300

    resolução do exercicio proposto

--- Course-of-Java-Nelio-Alves/exercices/exercice005.java
+++ Course-of-Java-Nelio-Alves/exercices/exercice005.java
@@ -2,8 +2,27 @@
  * Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
  */
 
+import java.util.Scanner;
+
 public class exercice005 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numeroDoFuncionario, horasTrabalhadas;
+        double salario;
 
+        System.out.print("Digite o número do funcionário: ");
+        numeroDoFuncionario = sc.nextInt();
+
+        System.out.print("Digite o número de horas trabalhadas: ");
+        horasTrabalhadas = sc.nextInt();
+
+        System.out.print("Digite o valor que este funcionário recebe por horas trabalhadas: ");
+        salario = sc.nextDouble();
+
+        System.out.printf("O colaborador com a matrícula %d terá o salário de R$ %.2f%n",
+                numeroDoFuncionario,
+                horasTrabalhadas * salario);
+
+        sc.close();
     }
 }
