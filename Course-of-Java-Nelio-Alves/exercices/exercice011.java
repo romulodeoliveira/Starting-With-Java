e2ff14c63987c2314b6b3ab13fe738469c34c1ab
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-02-15 23:24:11 -0300

    Resolução do exercício 05 sobre Estrutura Condicional (if-else)

---
+++ Course-of-Java-Nelio-Alves/exercices/exercice011.java
@@ -0,0 +1,21 @@
+/*
+ * Escreva um programa que leia o código de um item e a quantidade deste item. A seguir, calcule e mostre o valor da conta a pagar
+ */
+
+import java.util.Scanner;
+
+public class exercice011 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int code, quantity;
+        double price
+
+        System.out.print("Digite o código e a quantidade do item : ");
+        code = sc.nextInt();
+        quantity = sc.nextInt();
+
+
+        System.out.printf("O valor final é de %");
+        sc.close();
+    }
+}
