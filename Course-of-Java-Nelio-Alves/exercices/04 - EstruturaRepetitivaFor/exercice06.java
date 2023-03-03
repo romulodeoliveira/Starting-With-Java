7b027df57d622642f19d8cd4895b483690724fed
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-03-03 00:35:22 -0300

    resolução do exercicio 6 de estrutura repetitiva for

--- Course-of-Java-Nelio-Alves/exercices/04 - EstruturaRepetitivaFor/exercice06.java
+++ Course-of-Java-Nelio-Alves/exercices/04 - EstruturaRepetitivaFor/exercice06.java
@@ -2,6 +2,21 @@
  * Ler um número inteiro N e calcular todos os seus divisores. 
  */
 
-public class exercice06 {
+import java.util.Scanner;
 
+public class exercice06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            if (N % i == 0) {
+                System.out.printf("%d é divisível por %d.%n", N, i);
+            }
+        }
+
+        sc.close();
+    }
 }
