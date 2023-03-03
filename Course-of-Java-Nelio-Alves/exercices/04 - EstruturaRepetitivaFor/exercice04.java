351d1fb251b54ae556f1d02cf126f082aee4a9d3
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-03-03 00:35:03 -0300

    resolução do exercicio 4 de estrutura repetitiva for

--- Course-of-Java-Nelio-Alves/exercices/04 - EstruturaRepetitivaFor/exercice04.java
+++ Course-of-Java-Nelio-Alves/exercices/04 - EstruturaRepetitivaFor/exercice04.java
@@ -8,6 +8,26 @@ public class exercice04 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Digite um número: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Digite o primeiro número: ");
+            int a = sc.nextInt();
+
+            System.out.print("Digite o segundo número: ");
+            int b = sc.nextInt();
+
+            if (b == 0) {
+                System.out.println("Divisão impossível.");
+            }
+
+            else {
+                Double resultado = (double) a / b;
+                System.out.printf("O resultado é: %f%n%n", resultado);
+            }
+        }
+
         sc.close();
     }
 }
