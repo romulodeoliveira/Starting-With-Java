98174e18ba95fb71653562eae8c0e3e0a6d6520e
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-03-03 00:35:12 -0300

    resolução do exercicio 5 de estrutura repetitiva for

--- Course-of-Java-Nelio-Alves/exercices/04 - EstruturaRepetitivaFor/exercice05.java
+++ Course-of-Java-Nelio-Alves/exercices/04 - EstruturaRepetitivaFor/exercice05.java
@@ -2,6 +2,23 @@
  * Ler um valor N. Calcular e escrever seu respectivo fatorial. Fatorial de N = N * (N-1) * (N-2) * (N-3) * ... * 1. Lembrando que, por definição, fatorial de 0 é 1. 
  */
 
-public class exercice05 {
+import java.util.Scanner;
 
+public class exercice05 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um valor: ");
+        int N = sc.nextInt();
+
+        System.out.println("Fatorial:");
+        for (int i = 1; i < 5; i++) {
+            N = N * i;
+            System.out.printf("%d x %d | ", N, i);
+        }
+
+        System.out.printf("Resultado: %d%n", N);
+
+        sc.close();
+    }
 }
