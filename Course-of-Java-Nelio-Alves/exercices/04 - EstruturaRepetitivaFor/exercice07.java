db6b2c7932ea108d803c44c8270bb562490bb1e6
Romulo de Oliveira <romulodeoliveira1805@gmail.com>
2023-03-03 00:35:33 -0300

    resolução do exercicio 7 de estrutura repetitiva for

--- Course-of-Java-Nelio-Alves/exercices/04 - EstruturaRepetitivaFor/exercice07.java
+++ Course-of-Java-Nelio-Alves/exercices/04 - EstruturaRepetitivaFor/exercice07.java
@@ -2,6 +2,19 @@
  * Fazer um programa para ler um número inteiro positivo N. O programa deve então mostrar na tela N linhas, começando de 1 até N. Para cada linha, mostrar o número da linha, depois o quadrado e o cubo do valor, conforme exemplo.
  */
 
-public class exercice07 {
+import java.util.Scanner;
 
+public class exercice07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número: ");
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            System.out.printf("%d %.0f %.0f%n", i, Math.pow(i, 2), Math.pow(i, 3));
+        }
+
+        sc.close();
+    }
 }
